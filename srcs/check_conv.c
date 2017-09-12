06d649260bf6ead834ec5350d89da6d2bcdcb235
Damien FOUQUET <dfouquet@e3r1p2.42.fr>
2017-09-12 17:35:20 +0200

    sauvez willy

--- srcs/check_conv.c
+++ srcs/check_conv.c
@@ -6,41 +6,46 @@
 /*   By: atgerard <atgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/12 13:14:09 by atgerard          #+#    #+#             */
-/*   Updated: 2017/09/12 16:11:04 by dfouquet         ###   ########.fr       */
+/*   Updated: 2017/09/12 17:24:24 by dfouquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../ncludes/ft_prntf.h"
+#include "../includes/ft_prntf.h"
 
-int		check_conv(char c, va_list ap, char *str, char *type)
+int		check_conv_bis(const char *format, va_list ap, int *i, int *str)
 {
-	if (c == 's')
-		return (flag_s(ap, str, type));
-	if (c == 'S')
-		return (flag_S(ap, str, type));
-	if (c == 'p')
-		return (flag_p(ap, str, type));
-	if (c == 'd')
-		return (flag_d(ap, str, type));
-	if (c == 'D')
-		return (flag_D(ap, str, type));
-	if (c == 'i')
-		return (flag_i(ap, str, type));
-	if (c == 'o')
-		return (flag_o(ap, str, type));
-	if (c == 'O')
-		return (flag_O(ap, str, type));
-	if (c == 'u')
+	if (format[*i] == 'u')
 		return (flag_u(ap, str, type));
-	if (c == 'U')
+	if (format[*i] == 'U')
 		return (flag_U(ap, str, type));
-	if (c == 'x')
+	if (format[*i] == 'x')
 		return (flag_x(ap, str, type));
-	if (c == 'X')
+	if (format[*i] == 'X')
 		return (flag_X(ap, str, type));
-	if (c == 'c')
+	if (format[*i] == 'c')
 		return (flag_c(ap, str, type));
-	if (c == 'C')
+	if (format[*i] == 'C')
 		return (flag_C(ap, str, type));
-	return (0);
+	return (-1);
+}
+
+int		check_conv(const char *format, va_list ap, int *i, int *str)
+{
+	if (format[*i] == 's')
+		return (flag_s(ap, str, type));
+	if (format[*i] == 'S')
+		return (flag_S(ap, str, type));
+	if (format[*i] == 'p')
+		return (flag_p(ap, str, type));
+	if (format[*i] == 'd')
+		return (flag_d(ap, str, type));
+	if (format[*i] == 'D')
+		return (flag_D(ap, str, type));
+	if (format[*i] == 'i')
+		return (flag_i(ap, str, type));
+	if (format[*i] == 'o')
+		return (flag_o(ap, str, type));
+	if (format[*i] == 'O')
+		return (flag_O(ap, str, type));
+	return (check_conv_bis(format, ap, i, str));
 }
--- srcs/check_flag.c
+++ srcs/check_flag.c
@@ -6,24 +6,45 @@
 /*   By: atgerard <atgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/11 14:34:41 by atgerard          #+#    #+#             */
-/*   Updated: 2017/09/12 16:27:46 by dfouquet         ###   ########.fr       */
+/*   Updated: 2017/09/12 17:24:27 by dfouquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
+char	find_numb(const char *format, int *i, int *str, int n)
+{
+	str[n] = ft_atoi(format + *i);
+	while (format[*i] >= '0' && format[*i] <= '9')
+		*i++;
+}
+
+
 int     check_flag(const char *format, va_list ap, int *i, int *str);
 {
 	*i++;
+	if (format[*i] == '%')
+	{
+		ft_putchar('%');
+		return (1);
+	}
 	if (format[*i] == '#' || format[*i] == '0' || format[*i] == '-'
 			|| format[*i] == '+' || format[*i] == ' ')
 		str[0] = format[*i++];
 	if (format[*i] >= '0' && format[*i] <= '9')
-		return (flag_length(format, ap, i, 'n'));
+		find_numb(format, i, str, 1);
 	if (format[*i] == '.')
-		return (flag_deci(format, ap, i, 'n'));
+	{
+		*i++;
+		find_numb(format, i, str, 2);
+	}
 	if (format[*i] == 'h' || format[*i] == 'l' ||
 			format[*i] == 'j' || format[*i] == 'z')
-		return (flag_type(format, ap, i, " 0"));
-	return (check_conv(format, ap, i, "n0"));
+		str[3] = format[*i++];
+	if ((format[*i] == 'h' || format[*i] == 'l') && format[*i] == str[3])
+	{
+		*i++;
+		str[3] -= 32;
+	}
+	return (check_conv(format, ap, i, str));
 }
